428d27e3eeb0a30991be872090b915900e5378b2
Marcos Hernandez <marcos.hernandez@out.fidseguros.cl>
2025-11-20 19:14:56 -0500

    Fex

--- src/contacpime/helpers/products.helper.ts
+++ src/contacpime/helpers/products.helper.ts
@@ -1,5 +1,19 @@
 import { IProduct, IProductMotowork } from '../interfaces/products.interface';
 
+const CATEGORIAS_PRODUCTOS = {
+  1: 'REPUESTOS MOTOR',
+  2: 'REPUESTOS CHASIS',
+  3: 'REPUESTOS ELECTRICOS',
+  4: 'TORNILLERIA Y AFINES',
+  5: 'LUBRICANTES',
+  6: 'LLANTAS NEUMATICOS O RINES',
+  7: 'REPUESTOS ESTANDAR',
+  8: 'ACCESORIOS',
+  9: 'INDUMENTARIA',
+  10: 'SERVICIO TECNICO',
+  11: 'PASTAS Y BANDAS DE FRENO',
+};
+
 /**
  * Prepara un producto y lo retorna en el formato correcto
  * Para guardarlo en la base de datos.
@@ -10,7 +24,8 @@ export const prepareProduct = (products: IProduct[]): IProductMotowork[] => {
   const productList: IProductMotowork[] = [];
 
   for (const product of products) {
-    productList.push({
+    if (product?.smarca.toUpperCase() !== 'YAMAHA' && parseInt(product.msaldo ) > 0) {
+      productList.push({
         name: product?.nrecurso,
         model: product?.smodelo || product?.sreffabricante,
         state: 'Nueva',
@@ -31,13 +46,15 @@ export const prepareProduct = (products: IProduct[]): IProductMotowork[] => {
         banner: [],
         images: [],
         // reviews: [],
-        category: '',
+        category:
+          CATEGORIAS_PRODUCTOS[product?.igrupoinv] || product?.igrupoinv,
         description: product?.sdescrip,
         sku: product?.irecurso || '',
         type: 'product',
         active: true,
         msaldo: product?.msaldo || 0,
-    });
+      });
+    }
   }
 
   return productList;
